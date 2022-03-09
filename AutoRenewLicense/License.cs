371b297d93bee8b71964bb4f846e7ebbc6bfa05e
ZuoGuoli <qiushanrunjun@hotmail.com>
2022-03-09 11:08:27 +0800

    [feat] Replace script with building command line

---
+++ AutoRenewLicense/License.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Playwright;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AutoRenewLicense
+{
+    public static class License
+    {
+        public static string GenerateAlfFile(string _unityEditorExePath, string _alfFileOutputPath)
+        {
+            Process.Start();
+        }
+
+        public static async Task UploadAlfFile(string _filePath)
+        {
+
+        }
+    }
+}
